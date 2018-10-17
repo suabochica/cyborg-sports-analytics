0c0b9a114079b45a726fb57be553ac438b794db5
Javier Ospina <javierospina@ojyo.fritz.box>
2018-10-17 14:19:10 +1300

    Update src/components/Landing.vue
    cambie de datos por sobre datos

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="landing">
     <h1>{{ msg }}</h1>
-    <h4>Análisis complejo de datos aplicados en estrategias y entrenamientos deportivos.</h4>
+    <h4>Análisis complejo sobre datos aplicados en estrategias y entrenamientos deportivos.</h4>
     <section class="landing__section--mision">
       <p>Utilizamos la tecnología aplicada al deporte para analizar situaciones y rendimientos, permitiendo al usuario entender la situación actual y desarrollar estrategias a partir de estadísticas de datos complejos. A través de la innovación tecnología desarrollamos herramientas que generan una ventaja competitiva en el deporte y la vida. Nos fundamentamos en el esfuerzo y la pasión.</p>
     </section>
