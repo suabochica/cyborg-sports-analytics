677805bc47922906aeba0a6e2fe93ecdace2e066
Sergio L. Benítez D <sergio.kun21@gmail.com>
2019-01-02 21:59:47 -0500

    chore: add server blog example rename: rename files

---
+++ server/blogExample/blogServer.js
@@ -0,0 +1,18 @@
+import Express from 'express';
+import GraphHTTP from 'express-graphql';
+
+// Config
+const APP_PORT = 3000;
+
+// Start
+const app = Express();
+
+// GraphQL
+app.use('/graphql', GraphHTTP({
+  pretty: true,
+  graphiql: true,
+}));
+
+app.listen(APP_PORT, ()=> {
+  console.log(`App listening on port ${APP_PORT}`);
+});
