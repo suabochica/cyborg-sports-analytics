c48fe16cce117c64c3a89a5dfc94cb1c8dd7e979
Sergio L. Benítez D <sergio.kun21@gmail.com>
2019-02-04 23:24:48 -0500

    courses: update the explanation of the courses example including the fragment and the mutation functionalities of graphql.

--- client/src/views/Courses.vue
+++ client/src/views/Courses.vue
@@ -49,7 +49,121 @@
   }
 ]
       </pre>
-      <!--TODO: Agregar sección de las entradas y las salidas en la interfaz web de GraphQL-->
+      <h2>Consultas</h2>
+      <p>A continuación se van a mostrar las entradas y las salidas de los queries que se establecen con graphql.</p>
+      <p>Para recuperar un curso en particular.</p>
+      <p><i>entrada:</i></p>
+      <pre>
+query getSingleCourse($courseID: Int!) {
+  course(id: $courseID) {
+    title
+    author
+    description
+    topic
+    url
+  }
+}
+# Query Variables
+{
+  "courseID": 1
+}
+      </pre>
+      <p><i>salida:</i></p>
+      <pre>
+{
+  "data": {
+    "course": {
+      "title": "The Complete Node.js Developer Course",
+      "author": "Andrew Mead, Rob Percival",
+      "description": "Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!",
+      "topic": "Node.js",
+      "url": "https://codingthesmartway.com/courses/nodejs/"
+    }
+  }
+}
+      </pre>
+      <p>Para recuperar fragmentos de varios cursos utilizando la funcionalidad <code>fragment</code> de graphql.</p>
+      <p><i>entrada:</i></p>
+      <pre>
+query getCourseWithFragments($courseID1: Int!, $courseID2: Int!) {
+  course1: course(id: $courseID1) {
+    ...courseFields
+  }
+  course2: course(id: $courseID2) {
+    ...courseFields
+  }
+}
+
+fragment courseFields on Course {
+  title
+  author
+  description
+  topic
+  url
+}
+# Query Variables
+{
+  "courseID1": 2,
+  "courseID2": 3,
+}
+      </pre>
+      <p><i>salida:</i></p>
+      <pre>
+{
+  "data": {
+    "course1": {
+      "title": "Node.js, Express & MongoDB Dev to Deployment",
+      "author": "Brad Traversy",
+      "description": "Learn by example building & deploying real-world Node.js applications from absolute scratch",
+      "topic": "Node.js",
+      "url": "https://codingthesmartway.com/courses/nodejs-express-mongodb/"
+    },
+    "course2": {
+      "title": "JavaScript: Understanding The Weird Parts",
+      "author": "Anthony Alicea",
+      "description": "An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.",
+      "topic": "JavaScript",
+      "url": "https://codingthesmartway.com/courses/understand-javascript/"
+    }
+  }
+}
+      </pre>
+      <p>Para actualizar el tema de un curso en particular utilizando la funcionalidad <code>mutatution</code> de graphql.</p>
+      <p><i>entrada:</i></p>
+      <pre>
+mutation updateCourseTopic($id: Int!, $topic: String!) {
+  updateCourseTopic(id: $id, topic: $topic) {
+    ...courseFields
+  }
+}
+
+fragment courseFields on Course {
+  title
+  author
+  description
+  topic
+  url
+}
+# Query Variables
+{
+  "id": 1,
+  "topic": "Norick"
+}
+      </pre>
+      <p><i>salida:</i></p>
+      <pre>
+{
+  "data": {
+    "updateCourseTopic": {
+      "title": "The Complete Node.js Developer Course",
+      "author": "Andrew Mead, Rob Percival",
+      "description": "Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!",
+      "topic": "Norick",
+      "url": "https://codingthesmartway.com/courses/nodejs/"
+    }
+  }
+}
+      </pre>
     </section>
   </div>
 </template>
